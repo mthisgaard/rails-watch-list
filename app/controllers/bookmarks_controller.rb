c5097e8fbe4e1d8ffabdfde496ffb6dd1f067bd0
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-11 12:21:23 +0900

    write controller actions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,28 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
