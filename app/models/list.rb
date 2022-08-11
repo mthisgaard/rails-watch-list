3f9f5a75c5db5c6dc62f196f1921d9ba1759da03
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-11 16:06:40 +0900

    custom list image

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :delete_all
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
+  validates :image_url, presence: true
 end
