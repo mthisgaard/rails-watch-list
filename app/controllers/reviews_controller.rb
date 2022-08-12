aa2671b0cccfc067e9200dc1e84aa6e21f6ec853
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-12 16:15:17 +0900

    add review model

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @list = List.find(params[:list_id])
+    @review.list = @list
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
