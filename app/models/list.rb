3e51fd02ed46a9933f9e306f68a188620a867f50
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-12 13:57:46 +0900

    fixed image upload

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,4 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   has_one_attached :photo
   validates :name, presence: true, uniqueness: true
-  validates :image_url, presence: true
 end
